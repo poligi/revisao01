14bd9357741b53a482b455129e172a71cb6cbd71
Giovana Pollo <pollogiih2@gmail.comm>
2024-09-22 14:28:03 -0300

    erros corrigidos

--- ex01 resto/script.js
+++ ex01 resto/script.js
@@ -8,13 +8,13 @@ let numero2 = parseInt(prompt('Digite mais um:'))
 let numero3 = parseInt(prompt('O último:'))
 
       //situação 1
-let soma1 = numero1 ** numero2
+let soma1 = numero1 % numero2
 console.log(soma1)
 
        //situação 2
-let soma2 = numero2 ** numero3
+let soma2 = numero2 % numero3
 console.log(soma2)
 
        //situação 3
-let soma3 = numero1 ** numero3
+let soma3 = numero1 % numero3
 console.log(soma3)
